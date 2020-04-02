77231130b5e66b0ae99cf0f016b4e0fd28018ed8
Egor <redwoodx3@gmail.com>
2020-04-02 13:44:42 +0300

    add script and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-picture-automatization
+picture-automatization script
+automatically transfers files with extensions .png .jpg .jpeg .gif .webp .svg from selected folder to the destination folder
+
+in pict-automatization.py:
+old_folder = '*for example - path to download folder*'
+new_folder = '*path to destination folder*'
+
+run:
+1. install python 3.6+
+2. pip install watchdog
+3. pip install pyinstaller
+4. pyinstaller -F -w pict-automatization.py
+5. in foder dist run executable file
---
+++ pict-automatization.py
@@ -0,0 +1,34 @@
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+import time
+import os
+import shutil
+
+class Handler(PatternMatchingEventHandler):
+
+    def on_modified(self, event):
+        extensions = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp']
+
+        for filename in os.listdir(old_folder):
+            file = filename.split('.')
+            file_extension = file[-1].lower()
+            if file_extension in extensions:
+                old_place = old_folder + '/' + filename
+                new_place = new_folder + '/' + filename
+                shutil.move(old_place, new_place)
+
+
+old_folder = ''
+new_folder = ''
+
+handler = Handler()
+observer = Observer()
+observer.schedule(handler, old_folder, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
